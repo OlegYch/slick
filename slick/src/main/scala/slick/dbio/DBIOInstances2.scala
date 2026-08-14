cace4a6501388882a158a8fa109d7b78ebefb4b8
OlegYch <oleglbch@gmail.com>
2026-08-14 16:35:41 +0200

    try various ways of defining monad - all fail on all scalas

--- slick/src/main/scala/slick/dbio/DBIOInstances2.scala
+++ slick/src/main/scala/slick/dbio/DBIOInstances2.scala
@@ -1,69 +1,68 @@
 package slick.dbio
-trait DBIOInstances2
-//package slick.dbio
-//
-//import cats.*
-//import cats.syntax.all.*
-//
-//import scala.util.*
-//
-//trait DBIOInstances2 {
-//  implicit val dbioInstance: MonadError[[R] =>> DBIOAction[R, NoStream, Effect], Throwable] =
-//    new MonadError[[R] =>> DBIOAction[R, NoStream, Effect], Throwable] {
-//      override def pure[A](x: A): DBIOAction[A, NoStream, Effect] = DBIO.successful(x)
-//
-//      override def flatMap[A, B](fa: DBIOAction[A, NoStream, Effect])(f: (A) => DBIOAction[B, NoStream, Effect]): DBIOAction[B, NoStream, Effect] = fa.flatMap(f)
-//
-//      /**
-//       * While this is roughly the same implementation as in `FutureInstances`,
-//       * I'm not entirely sure this is indeed stack safe. It certainly looks
-//       * like it should be.
-//       */
-//      override def tailRecM[A, B](a: A)(f: A => DBIOAction[Either[A, B], NoStream, Effect]): DBIOAction[B, NoStream, Effect] =
-//        f(a).flatMap {
-//          case Left(a1) => tailRecM(a1)(f)
-//          case Right(b) => DBIO.successful(b)
-//        }
-//
-//      override def handleError[A](fea: DBIOAction[A, NoStream, Effect])(f: (Throwable) => A): DBIOAction[A, NoStream, Effect] =
-//        fea.asTry.map {
-//          case Success(a) => a
-//          case Failure(t) => f(t)
-//        }
-//
-//      override def raiseError[A](e: Throwable): DBIOAction[A, NoStream, Effect] = DBIO.failed(e)
-//
-//      override def map[A, B](fa: DBIOAction[A, NoStream, Effect])(f: A => B): DBIOAction[B, NoStream, Effect] = fa.map(f)
-//
-//      override def handleErrorWith[A](fa: DBIOAction[A, NoStream, Effect])(f: (Throwable) => DBIOAction[A, NoStream, Effect]): DBIOAction[A, NoStream, Effect] =
-//        fa.asTry.flatMap {
-//          case Success(a) => DBIO.successful(a)
-//          case Failure(t) => f(t)
-//        }
-//    }
-//
-//  implicit def dbioGroup[A: Group]: Group[DBIOAction[A, NoStream, Effect]] =
-//    new DBIOGroup[A]
-//
-//  implicit def dbioMonoid[A: Monoid]: Monoid[DBIOAction[A, NoStream, Effect]] =
-//    new DBIOMonoid[A]
-//
-//  implicit def dbioSemigroup[A: Semigroup]: Semigroup[DBIOAction[A, NoStream, Effect]] =
-//    new DBIOSemigroup[A]
-//
-//  private class DBIOSemigroup[A: Semigroup] extends Semigroup[DBIOAction[A, NoStream, Effect]] {
-//    override def combine(fx: DBIOAction[A, NoStream, Effect], fy: DBIOAction[A, NoStream, Effect]): DBIOAction[A, NoStream, Effect] =
-//      (fx zip fy).map { case (x, y) => x |+| y }
-//  }
-//
-//  private class DBIOMonoid[A](implicit A: Monoid[A]) extends DBIOSemigroup[A] with Monoid[DBIOAction[A, NoStream, Effect]] {
-//    def empty: DBIOAction[A, NoStream, Effect] = DBIO.successful(A.empty)
-//  }
-//
-//  private class DBIOGroup[A](implicit A: Group[A]) extends DBIOMonoid[A] with Group[DBIOAction[A, NoStream, Effect]] {
-//    def inverse(fx: DBIOAction[A, NoStream, Effect]): DBIOAction[A, NoStream, Effect] = fx.map(_.inverse())
-//
-//    override def remove(fx: DBIOAction[A, NoStream, Effect], fy: DBIOAction[A, NoStream, Effect]): DBIOAction[A, NoStream, Effect] =
-//      (fx zip fy).map { case (x, y) => x |-| y }
-//  }
-//}
+
+import cats.*
+import cats.syntax.all.*
+
+import scala.util.*
+
+trait DBIOInstances2 {
+  type DBIOAll[R] = DBIOAction[R, NoStream, Effect]
+  implicit val dbioInstance2: MonadError[DBIOAll, Throwable] =
+    new MonadError[DBIOAll, Throwable] {
+      override def pure[A](x: A): DBIOAll[A] = DBIO.successful(x)
+
+      override def flatMap[A, B](fa: DBIOAll[A])(f: (A) => DBIOAll[B]): DBIOAll[B] = fa.flatMap(f)
+
+      /**
+       * While this is roughly the same implementation as in `FutureInstances`,
+       * I'm not entirely sure this is indeed stack safe. It certainly looks
+       * like it should be.
+       */
+      override def tailRecM[A, B](a: A)(f: A => DBIOAll[Either[A, B]]): DBIOAll[B] =
+        f(a).flatMap {
+          case Left(a1) => tailRecM(a1)(f)
+          case Right(b) => DBIO.successful(b)
+        }
+
+      override def handleError[A](fea: DBIOAll[A])(f: (Throwable) => A): DBIOAll[A] =
+        fea.asTry.map {
+          case Success(a) => a
+          case Failure(t) => f(t)
+        }
+
+      override def raiseError[A](e: Throwable): DBIOAll[A] = DBIO.failed(e)
+
+      override def map[A, B](fa: DBIOAll[A])(f: A => B): DBIOAll[B] = fa.map(f)
+
+      override def handleErrorWith[A](fa: DBIOAll[A])(f: (Throwable) => DBIOAll[A]): DBIOAll[A] =
+        fa.asTry.flatMap {
+          case Success(a) => DBIO.successful(a)
+          case Failure(t) => f(t)
+        }
+    }
+
+  implicit def dbioGroup2[A: Group]: Group[DBIOAll[A]] =
+    new DBIOGroup[A]
+
+  implicit def dbioMonoid2[A: Monoid]: Monoid[DBIOAll[A]] =
+    new DBIOMonoid[A]
+
+  implicit def dbioSemigroup2[A: Semigroup]: Semigroup[DBIOAll[A]] =
+    new DBIOSemigroup[A]
+
+  private class DBIOSemigroup[A: Semigroup] extends Semigroup[DBIOAll[A]] {
+    override def combine(fx: DBIOAll[A], fy: DBIOAll[A]): DBIOAll[A] =
+      (fx zip fy).map { case (x, y) => x |+| y }
+  }
+
+  private class DBIOMonoid[A](implicit A: Monoid[A]) extends DBIOSemigroup[A] with Monoid[DBIOAll[A]] {
+    def empty: DBIOAll[A] = DBIO.successful(A.empty)
+  }
+
+  private class DBIOGroup[A](implicit A: Group[A]) extends DBIOMonoid[A] with Group[DBIOAll[A]] {
+    def inverse(fx: DBIOAll[A]): DBIOAll[A] = fx.map(_.inverse())
+
+    override def remove(fx: DBIOAll[A], fy: DBIOAll[A]): DBIOAll[A] =
+      (fx zip fy).map { case (x, y) => x |-| y }
+  }
+}
